b7c5395a56677bc1a97129ca79007de6340a4a4c
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-02-02 21:19:34 +0900

    refactaring

--- src/calculator.ts
+++ src/calculator.ts
@@ -8,7 +8,7 @@ export class Calculator {
       return 'Error: 引数が多すぎます';
     }
 
-    if (args.some(arg => isNaN(Number(arg)))) {
+    if (args.slice(1).some(arg => isNaN(Number(arg)))) {
       return 'Error: 数値以外の引数が含まれています';
     }
 
